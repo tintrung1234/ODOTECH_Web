de447661c52f6ad24681855edda3cd5852009337
tintrung <huynhtrungtin1506@gmail.com>
2025-12-30 21:39:28 +0700

    fix build

--- ODOTECH_Frontend/src/utils/projectUtils.ts
+++ ODOTECH_Frontend/src/utils/projectUtils.ts
@@ -1,4 +1,4 @@
-import type { ProjectPriority, ProjectStatus, ProjectMgmtPriority, ProjectMgmtStatus } from './interface/type';
+import type { ProjectPriority, ProjectStatus, ProjectMgmtPriority, ProjectMgmtStatus } from '../components/projectsDasboard/interface/type';
 
 export function statusLabel(status: ProjectStatus | ProjectMgmtStatus) {
   if (status === 'not_started') return 'Chưa bắt đầu';
